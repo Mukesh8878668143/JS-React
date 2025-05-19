ec15cf0f4827c5f433b4f29f90eb0618a64121b8
Mukesh8878668143 <48101714+Mukesh8878668143@users.noreply.github.com>
2025-05-19 23:17:16 +0530

    Changes of Useeffact

--- IMDPRating/Project01/src/Component/Banner.jsx
+++ IMDPRating/Project01/src/Component/Banner.jsx
@@ -1,30 +1,28 @@
+<<<<<<< HEAD
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+=======
 import React, { useState, useEffect } from 'react';
 import Poster1 from '../assets/Poster.png';
 import Poster2 from '../assets/poster2.jpg';
 import Poster3 from '../assets/Poster3.jpg';
 import GodzillaPoster from '../assets/godzilla-vs-kong-poster04.jpg';
-import axios from 'axios';
 
 const Banner = () => {
+    const [movies,setmovies]=useState([]);
+    useEffect(()=>{
+        axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
+        .then(function(response){
+            setmovies(response.data.result);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    })
     const images = [Poster1, Poster2, Poster3, GodzillaPoster];
 
     const[currentIndex, setCurrentIndex] = useState(0);
-
-    const [movies, setmovies] = useState([]);
-
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=5d141214fd2c717673a888e6ab2e6ccb');
-                setmovies(response.data.results);
-                console.log(response.data.results);
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
-        };
-
-        fetchMovies();
-    }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -33,8 +31,6 @@ const Banner = () => {
 
         return () => clearInterval(interval); // Cleanup on unmount 
     },[images.length]);
-
-
 
     return (
         <div className='flex flex-col items-center justify-center bg-gray-800 text-white py-10'>
