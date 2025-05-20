e82e9158e62c2c7d0a974785e572b5c9445903ad
Mukesh8878668143 <48101714+Mukesh8878668143@users.noreply.github.com>
2025-05-20 22:44:05 +0530

    Revert "Axios"
    This reverts commit d5064076cb351f3bac746af123dde34038f30d06.

--- IMDPRating/Project01/src/Component/Banner.jsx
+++ IMDPRating/Project01/src/Component/Banner.jsx
@@ -1,30 +1,32 @@
+<<<<<<< HEAD
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+=======
 import React, { useState, useEffect } from 'react';
 import Poster1 from '../assets/Poster.png';
 import Poster2 from '../assets/poster2.jpg';
 import Poster3 from '../assets/Poster3.jpg';
 import GodzillaPoster from '../assets/godzilla-vs-kong-poster04.jpg';
-import axios from 'axios';
 
+>>>>>>> c128bf45db949884cc05c17d6d72546dfc0107e3
 const Banner = () => {
+<<<<<<< HEAD
+
+    const [movies,setmovies]=useState([]);
+    useEffect(()=>{
+        axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
+        .then(function(response){
+            setmovies(response.data.result);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    })
+=======
     const images = [Poster1, Poster2, Poster3, GodzillaPoster];
 
     const[currentIndex, setCurrentIndex] = useState(0);
-
-    const [movies, setmovies] = useState([]);
-
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=5d141214fd2c717673a888e6ab2e6ccb');
-                setmovies(response.data.results);
-                console.log(response.data.results);
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
-        };
-
-        fetchMovies();
-    }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -34,13 +36,24 @@ const Banner = () => {
         return () => clearInterval(interval); // Cleanup on unmount 
     },[images.length]);
 
-
-
+>>>>>>> c128bf45db949884cc05c17d6d72546dfc0107e3
     return (
+<<<<<<< HEAD
+        <div className="banner">
+            <div className='text-2xl fonrt bold text-center m-4'>
+                <h1>Trending Movies</h1>
+            </div>
+            <div>
+                {/* {movies.map((movieobj,i)=>{
+                    return <MovieCard movieobj = {movieobj}></>
+                })} */}
+            </div>
+=======
         <div className='flex flex-col items-center justify-center bg-gray-800 text-white py-10'>
             <h1>Poster</h1>
         <img src={images[currentIndex]} alt={`Slide ${currentIndex}`}
         className="w-full max-w-md rounded-lg shadow-lg" />
+>>>>>>> c128bf45db949884cc05c17d6d72546dfc0107e3
         </div>
     );
 }
